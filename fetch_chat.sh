bdf772e55fe25f4b962103afbf869a3c8b080da4
malinjr07 <malinjr07@gmail.com>
2026-07-11 19:35:43 +0600

    feat: add fetch_chat.sh script to automate telegram channel downloads

---
+++ fetch_chat.sh
@@ -0,0 +1,28 @@
+echo "Faceless Francis Group"
+uv run telegram-download-chat 3051045515
+
+echo "AI Hustlers"
+uv run telegram-download-chat @aihustlersgc --subchat "general"
+
+echo "Stacked Creators"
+uv run telegram-download-chat @stackedcreators --subchat "Stacked"
+
+echo "Doritoz"
+uv run telegram-download-chat 3886593329 --subchat "General"
+uv run telegram-download-chat 3886593329 --subchat "Dev Talk"
+
+echo "Hive Mind"
+uv run telegram-download-chat @HiveMind_OFM
+
+echo "ComfyOFM"
+uv run telegram-download-chat @ComfyOFM
+
+echo "AI_MODEL_ACADEMY"
+uv run telegram-download-chat 4308260628 --subchat "General Chat"
+uv run telegram-download-chat 4308260628 --subchat "Social Media Questions"
+
+echo "AI_OFM_FRAMEWORK"
+uv run telegram-download-chat 3929008719 --subchat "CHAT"
+
+echo "SKY"
+uv run telegram-download-chat @skyloras
