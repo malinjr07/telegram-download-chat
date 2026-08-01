0b7c2b18611165402d37a7803099b4f113ad015f
Mosaddik Ali Maruf <malinjr07@gmail.com>
2026-08-02 01:54:12 +0600

    Reorder priorities

--- fetch_chat.sh
+++ fetch_chat.sh
@@ -4,18 +4,8 @@ uv run telegram-download-chat 3051045515
 echo "AI Hustlers"
 uv run telegram-download-chat @aihustlersgc --subchat "general"
 
-echo "Stacked Creators"
-uv run telegram-download-chat @stackedcreators --subchat "General"
-
-echo "Doritoz"
-uv run telegram-download-chat 3886593329 --subchat "General"
-uv run telegram-download-chat 3886593329 --subchat "Dev Talk"
-
 echo "Hive Mind"
 uv run telegram-download-chat @HiveMind_OFM
-
-echo "ComfyOFM"
-uv run telegram-download-chat @ComfyOFM
 
 echo "AI_MODEL_ACADEMY"
 uv run telegram-download-chat 4308260628 --subchat "General Chat"
@@ -23,6 +13,16 @@ uv run telegram-download-chat 4308260628 --subchat "Social Media Questions"
 
 echo "AI_OFM_FRAMEWORK"
 uv run telegram-download-chat 3929008719 --subchat "CHAT"
+
+echo "Stacked Creators"
+uv run telegram-download-chat @stackedcreators --subchat "General"
+
+echo "Doritoz"
+uv run telegram-download-chat 3886593329 --subchat "General"
+uv run telegram-download-chat 3886593329 --subchat "Dev Talk"
+
+echo "ComfyOFM"
+uv run telegram-download-chat @ComfyOFM
 
 echo "SKY"
 uv run telegram-download-chat "SKY AI OFM"
